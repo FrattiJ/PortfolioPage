d48d7f0ab117bc6dd2a2c4ba70f9d6ac482b1a1d
Jacob Fratti <JacobRFratti@gmail.com>
2024-01-29 15:16:43 -0500

    Need css help for projects section, but jsx done

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { FaDesktop, FaGithub } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 
-const ProjectItem = ({img, title}) => {
+const ProjectItem = ({image, title, git}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-lg shadow-gray-400 rounded-xl group hover:bg-gradient-to-b from-gray-200 to-gray-400/80'>
-        <img src={img} alt={title} className="rounded-xl group-hover:opacity-10" />
+        <img src={image} alt={title} className="rounded-xl group-hover:opacity-10" />
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] traslate-y-[-50%]'>
             <h3 className='text-3xl font-bold text-white tracking-wider text-center'>
                 {title}
             </h3>
-            <div className='relative flex justify-between items-center cursor-pointer p-6 w-full'>
-                <a href='' className='p-3'>
+            <div className='relative flex justify-center items-center cursor-pointer p-6 w-full'>
+                <a href={git} className='p-3'>
                     <FaGithub size={25} />
-                </a>
-                <a href='' className='p-3'>
-                    <FaDesktop size={25} />
                 </a>
             </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
 import ProjectItem from './ProjectItem'
-// import images from assets for projects
-import Vm from '../assets/Volleyball Meme.jpeg'
+
+const items = [
+    {
+        image:'src/assets/Weather Wayz.gif',
+        title:'Weather Wayz',
+        git:'',
+    },
+    {
+        image:'src/assets/sense-2326348_1280.jpg',
+        title:'Personality Based Chat',
+        git:'',
+    },
+    {
+        image:'src/assets/Horiseon.gif',
+        title:'Horiseon',
+        git:'',
+    },
+    {
+        image:'src/assets/YouveBeenFrogged.jpg',
+        title:'Place Holder',
+        git:'',
+    },
+]
 
 const Projects = () => {
   return (
-    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4'>
         <h1 className='text-4xl font-bold text-center text-gray-800'>Projects</h1>
         <p className='text-center py-8'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error praesentium inventore sapiente nobis dicta alias molestiae, aperiam ipsa, aliquid rerum, obcaecati quaerat voluptatibus debitis sunt minus nam facere consectetur vitae.
         </p>
         <div className='grid sm:grid-col-2 gap-12'>
-            <ProjectItem img={Vm} title='Test' />
-            {/* <ProjectItem img={} title='' />
-            <ProjectItem img={} title='' />
-            <ProjectItem img={} title='' />
-            <ProjectItem img={} title='' /> */}
+            {items.map((items, idx) => (
+                <ProjectItem key={idx} image={items.image} title={items.title} git={items.git}/>
+            ))}
         </div>
     </div>
   )
