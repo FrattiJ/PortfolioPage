8d2fa9de70e1d401fd877c52fdad5a9620cf6f47
Jacob Fratti <JacobRFratti@gmail.com>
2024-01-28 20:44:43 -0500

    resume code done, need the information to add

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,12 +5,12 @@ const Main = () => {
   return (
     <div id='main'>
         <img className='w-full h-screen object-cover scale-x-[1] z-[-1]' 
-        src='/Volleyball Meme.jpeg'/>
+        src='src/assets/Volleyball Meme.jpeg'/>
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center'>
                 <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Jacob S. Rahmann Fratti</h1>
                 {/* Fill in place holder */}
-                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-700'>Place Holder for a breif cover letter type deal</h2>
+                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-600'>Place Holder for a breif cover letter type deal</h2>
                 <div className='flex justify-between items-center pt-6 max-w-[200px] w-full'>
                     {/* Need to add the corrisponding anchor links */}
                     <FaLinkedin size={25} className='cursor-pointer' href=''/>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import ResumeItem from './ResumeItem'
+
+const items = [
+    {
+        year: 2020,
+        title: 'Southern Glazers Wine & Spirits',
+        duration: 'Current',
+        details:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        year: 2018,
+        title: 'FGCU Communication Department',
+        duration: '1.5 Years',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        year: 2019,
+        title: 'Gartner Inc.',
+        duration: '6 Months',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        year: 2017,
+        title: "FGCU Office of Housing",
+        duration: '2 Years',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+]
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div id='resume' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='text-4xl font-bold text-center text-gray-800'>Resume</h1>
+        {items.map((item, idx) => (
+            <ResumeItem key={idx} year={item.year} title={item.title} duration={item.duration} details={item.details}/>
+        ))}
+    </div>
   )
 }
 
